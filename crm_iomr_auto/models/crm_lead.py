fdad81b5b2c2d125c0f7006cdedcbe296e762ff2
rodrigo <rodrigomadu85@gmail.com>
2026-04-20 16:05:27 -0300

    incluso campos da tabela de importação

--- crm_iomr_auto/models/crm_lead.py
+++ crm_iomr_auto/models/crm_lead.py
@@ -7,3 +7,11 @@ class CRMLead(models.Model):
 
     referred_partner = fields.Many2many('res.partner', relation='crmlead_rel_res_partner', column1='lead_id', column2='partner_id', string='Indicações', copy=False, domain=[('type_partner', '!=', 'convenio')])
     convenio = fields.Many2one('res.partner', string='Convênio', domain=[('type_partner', '=', 'convenio')])
+    doctor = fields.Many2one('res.partner', string='Doctor', domain=['|',('type_partner', '=', 'doctorext'),('type_partner', '=', 'doctorint')])
+    procedure = fields.Char(string='Procedure')
+    id_orc = fields.Integer(string='ID do Orçamento')
+    date = fields.Date(strign='Data')
+    date_contact = fields.Date(string='Data do Contato')
+    motives = fields.Char(string='Motives')
+    state_klingo = fields.Char(string='State Klingo')
+    
