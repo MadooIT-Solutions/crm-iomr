1f7d1609786a8016ad8581efe183c731524730fd
rodrigo <rodrigomadu85@gmail.com>
2026-04-20 13:01:37 -0300

    corrigido visualizações

--- crm_iomr_auto/models/crm_lead.py
+++ crm_iomr_auto/models/crm_lead.py
@@ -5,4 +5,5 @@ class CRMLead(models.Model):
     """Extends crm.lead for adding more functions in it"""
     _inherit = 'crm.lead'
 
-    referred_partner = fields.Many2many('res.partner', relation='crmlead_rel_res_partner', column1='lead_id', column2='partner_id', string='Indicações', copy=False)
+    referred_partner = fields.Many2many('res.partner', relation='crmlead_rel_res_partner', column1='lead_id', column2='partner_id', string='Indicações', copy=False, domain=[('type_partner', '!=', 'convenio')])
+    convenio = fields.Many2one('res.partner', string='Convênio', domain=[('type_partner', '=', 'convenio')])
--- crm_iomr_auto/models/res_partner.py
+++ crm_iomr_auto/models/res_partner.py
@@ -5,4 +5,4 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     type_partner = fields.Selection([('patient', 'Paciente'), ('doctorint', 'Médico Interno'),('doctorext','Médico Externo'),('employee', 'Funcionário'),
-                                     ('supplier', 'Fornecedor'),('gov','Governamental'),('others', 'Outros')], string='Tipo de Contato', default='patient')
+                                     ('supplier', 'Fornecedor'),('convenio','Convênio'),('gov','Governamental'),('others', 'Outros')], string='Tipo de Contato', default='others')
