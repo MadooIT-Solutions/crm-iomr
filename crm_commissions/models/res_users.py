2c885b15cd622816106de924a170c6780b27775a
rodrigo <rodrigomadu85@gmail.com>
2026-07-14 23:06:53 -0300

    [FIX]Adicionado pode ver valores, termos. Filtro de data.

--- crm_commissions/models/res_users.py
+++ crm_commissions/models/res_users.py
@@ -6,6 +6,9 @@ from odoo import api, fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
+    show_repasse_table = fields.Boolean(related='partner_id.show_repasse_table')
+    show_all_values = fields.Boolean(related='partner_id.show_all_values')
+
     crm_role = fields.Selection(
         selection=[
             ("sdr", "SDR"),
